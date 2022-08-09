2480b3b8b8f49f8aef06161273d677155436bc86
Hakimi keerai <hakeemee222@gmail.com>
2022-08-09 22:26:29 +0700

    props

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
+
+import CharacterCard from "./CharacterCard";
 function App() {
-  return <div>Hello {"world"}</div>;
+  return (
+    <div>
+      <CharacterCard value="h" />
+      <CharacterCard value="i" />
+    </div>
+  );
 }
 export default App;
