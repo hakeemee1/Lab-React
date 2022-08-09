43c39a69c6755003dde270e41c1e6e04b20d5ea0
Hakimi keerai <hakeemee222@gmail.com>
2022-08-09 22:23:32 +0700

    Hello world with JSX

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 function App() {
- return (
- <div>
- Hello World
- </div>
- );
+  return <div>Hello {"world"}</div>;
 }
 export default App;
