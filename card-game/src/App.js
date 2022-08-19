d991ee0fc7ca8e4424a29c4cdbb0cd6b743b8584
Hakimi keerai <hakeemee222@gmail.com>
2022-08-19 17:31:27 +0700

    explain about the button link to the game

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,31 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Timer from "./Timer";
 
 import WordCard from "./WordCard";
 
 function App() {
-    const [isShown, setIsShown] = useState(false);
-    const [ready, setReady] = useState("Click me to start!!");
-    const handleClick = event => {
-        // 👇️ toggle shown state
-        setIsShown(current => !current);
-    
-        // 👇️ or simply set it to true
-        // setIsShown(true);
-      };
-    
- return (
- <div className="game">
- <WordCard value="MSSPSU"/>
- <button className="ready" onClick={()=>{
-    setReady("Game started");
-    console.log("Game started and please enjoy ^^");
-    
- }}>{ready}</button>
-{isShown && <Timer />}
- </div>
- );
+  const [isShown, setIsShown] = useState(false);
+  const [ready, setReady] = useState("Click me to start!!");
+  const handleClick = (event) => {
+    setIsShown((current) => !current);
+  };
+
+  return (
+    <div className="game">
+      <WordCard value="MSSPSU" />
+      <button
+        className="ready"
+        onClick={() => {
+          setReady("Game started");
+          console.log("Game started and please enjoy ^^");
+        }}
+      >
+        {ready}
+      </button>
+      {isShown && <Timer />}
+    </div>
+  );
 }
 
 export default App;
